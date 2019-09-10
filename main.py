f8c46b838938d8d1a16bde77fff247f776f2e0ec
Caglan <cakpinar23@gmail.com>
2019-09-10 19:06:30 +0300

    commenting main.py
    This project needs commenting in order to make the process more clear

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ import constants
 
 # parameters shapes how we process CPC, CTR and reach prediction
 parameters = {
+    # this path must have .csv file it has data has CTR, CPS, reach and other features for prediction.
+    # Ex: pls check sem_bidding.csv file
     'path': None,
+    # this allows us the train our model again. If no need assign False
+    # training process takes about 1 hour. Make sure have 1.7gb free ram before train model. It also works on h20.
     'model_train': False,
+    # This also takes half an hour. It runs the model with combined features in order to find the optimum solutions
     'predict': False,
+    # output of the project assigning on output dictionary.
     'output': {
         'cpc': {'model_path': None, 'predicted': None,
                 'model_features': {'num': None , 'cat': None}, 'best_model': None},
@@ -19,14 +25,20 @@ parameters = {
         'reach': {'model_path': None, 'predicted': None,
                   'model_features': {'num': None , 'cat': None}, 'best_model': None}
     },
+    # These are the categorical features of our model
     'comb_cat_columns': ['aggresive_level', 'bid_limits', 'content', 'pop_level',
                          'strategy', 'time_period', 'keyword_1', 'keyword_2'],
+    # These are numerical features of our mode
     'comb_num_columns': ['impression', 'rpo'],
+    # prediction data frame assigning
     'pred_data': None,
+    # These are unique values of model features. We are using them as filters on our dashboard
     'dashboard_filters': None,
+    # Combination of sample data set has 8 millions of rows. That is why we are running the in a batch size and
+    # appending them in an array
     'prediction_batch_size': 200000,
+    # This returns optimum solution for our sample data set. Minimum cost max revenue
     'solution': None
-
 }
 
 def main(params):
@@ -34,17 +46,26 @@ def main(params):
     print("trained models are detected!") if not params['model_train'] else print("let`s train CTR, CPC and reach")
     print("predicted values are detected!") if not params['predict'] else print("let`s predict")
 
+    # gather data from .csv.
     data = data_access.get_data_from_csv(params)
+    # doing manipulations for parsing keywords and calculating total cost and revenue for each rows.
     data = data_manipulation.calculate_total_cost_revenue_conversion(data)
+    # for sample data 8 million gathers combinations of each categorical features
     params['pred_data'], params['dashboard_filters'], iters = prediction.combination_data_preparation(data, params)
 
+    # start prediction or training model procces of each metric
     for y in params['output']:
+        # if we assign model path is gathers model from there. Othervise path is assigned in constans.
         _path = params['output'][y]['model_path'] if params['output'][y][
                                                          'model_path'] is not None else constants.model_save_path
 
         if params['model_train']:
+            # inputs for training process
             X = constants.model_features[y] if y['model_features']['X']['numeric'] is None else y['model_features']['X']
+            # this is for one-hot encoding for categorical features.
             _data = data_manipulation.converting_numeric_encoder(data, X)
+            # each learning process assign for GGM, DRF, DNN, GLM machine Learning models.
+            # At the end H2o allows us to find the best model by using stack Ensemble model
             model_train.best_prediction_model(_data, constants.search_criteria,
                                               constants.hyper_p_gbm, constants.hyper_p_drf,
                                               constants.hyper_p_dnn,constants.hyper_p_glm
@@ -53,15 +74,19 @@ def main(params):
             model_train.best_prediction_model.compute_best_model()
             params['output'][y]['best_model'] = model_train.best_prediction_model.best_model
             h2o.save_model(model=params['output'][y]['best_model'], path=_path, force=True)
+            # shot down h2o instance. This project it runs on each available cores on your server or local comp.
             h2o.shutdown(prompt=False)
 
         if params['predict']:
+            # prediction is initializing with trained model. batch size is crucial point at here
             params['output'][y]['predicted'] = prediction.get_prediction(params['output'][y]['model_path'],
                                                                          iters,
                                                                          params['pred_data'],
                                                                          params['prediction_batch_size'])
+            # writing on a pickle the prediction values as array
             data_access.pred_write_reader(_path, y, True, params)
         else:
+            # check existing path has the predictions
             params['output'][y]['predicted'] = data_access.pred_write_reader(_path, y, False, [])
 
 if __name__ == '__main__':
